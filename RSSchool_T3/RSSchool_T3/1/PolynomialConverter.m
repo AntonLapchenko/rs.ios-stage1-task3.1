d447690698e5efff6e2acf2a63a5f08c7437d2e0
Anton <tatyana.shut.solveit@gmail.com>
2020-04-05 15:32:59 +0300

    feat: add Task1

--- RSSchool_T3/RSSchool_T3/1/PolynomialConverter.m
+++ RSSchool_T3/RSSchool_T3/1/PolynomialConverter.m
@@ -2,6 +2,59 @@
 
 @implementation PolynomialConverter
 - (NSString*)convertToStringFrom:(NSArray <NSNumber*>*)numbers {
-    return @"";
+    NSMutableString *result = [NSMutableString new];
+    NSString *sign = [NSString new];
+    NSString *koeffString = [NSString new];
+    NSString *degreeString = [NSString new];
+    NSString *xDegree = [NSString new];
+    NSInteger koeff = 0;
+    
+    if (numbers.count == 0) {
+        return nil;
+    } else {
+        NSUInteger maxDegree = numbers.count - 1;
+        for (NSUInteger i = 0; i < numbers.count; i++) {
+            koeff = [numbers[i] integerValue];
+            if (koeff > 0) {
+                sign = @" + ";
+            }
+            if (koeff < 0) {
+                sign = @" - ";
+            }
+            if (koeff == 0 && koeff < 0) {
+                sign = @" - ";
+            }
+            if (i == 0 && koeff > 0) {
+                sign = @"";
+            }
+            koeff = labs(koeff);
+            if  (koeff == 1) {
+                koeffString = @"";
+            }
+            if (koeff == 0) {
+                [result appendString:@""];
+                maxDegree --;
+                continue;
+            }
+            if (koeff > 1) {
+                koeffString = [NSString stringWithFormat:@"%ld", (long)koeff];
+            }
+            if (maxDegree == 1) {
+                xDegree = @"x";
+                degreeString = @"";
+            }
+            if (maxDegree == 0) {
+                xDegree = @"";
+                degreeString = @"";
+            }
+            if (maxDegree > 1) {
+                xDegree = @"x^";
+                degreeString = [NSString stringWithFormat:@"%lu", (unsigned long)maxDegree];
+            }
+            maxDegree --;
+            [result appendString:[NSString stringWithFormat:@"%@%@%@%@",sign, koeffString,xDegree, degreeString]];
+        }
+        return result;
+    }
 }
 @end
--- RSSchool_T3/RSSchool_T3/2/Combinator.m
+++ RSSchool_T3/RSSchool_T3/2/Combinator.m
@@ -5,3 +5,53 @@
     return @0;
 }
 @end
+
+//function resultFunction(arr) {
+//let resString = "";
+//if (arr.length === 0)
+//return null;
+//else {
+//let sign, koeff, koeffString, xDegree, degreeString;
+//let degree = arr.length - 1;
+//for (let i = 0; i < arr.length; i++) {
+//koeff = arr[i];// получили коэффициент перед Х
+//// разбираемся со знаками
+//if (koeff > 0)
+//sign = " + ";
+//if (koeff < 0)
+//sign = " - ";
+//if (i === 0 && koeff < 0)
+//sign = " - ";
+//if (i === 0 && koeff > 0)
+//sign = "";
+//// разбираемся с коэффициентом
+//koeff = Math.abs(koeff);
+//if (koeff === 1)
+//koeffString = "";
+//if (koeff === 0) {
+//resString += "";
+//degree--;
+//continue;
+//}
+//if (koeff > 1)
+//koeffString = (String)(koeff);
+////разбираемся с х в степени
+//if (degree === 1) {
+//xDegree = "x";
+//degreeString = "";
+//}
+//if (degree === 0) {
+//xDegree = "";
+//degreeString = "";
+//}
+//if (degree > 1) {
+//xDegree = "x^";
+//degreeString = (String)(degree);
+//}
+//degree--;
+//resString += sign + koeffString + xDegree + degreeString;
+//
+//}
+//return resString;
+//}
+//}
