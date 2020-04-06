f7906058e4e2cecc1038ed977eb382040e66af50
Anton <tatyana.shut.solveit@gmail.com>
2020-04-06 12:19:14 +0300

    feat: Add Task3

--- RSSchool_T3/RSSchool_T3/3 /ViewController.m
+++ RSSchool_T3/RSSchool_T3/3 /ViewController.m
@@ -1,6 +1,6 @@
 #import "ViewController.h"
 
-@interface ViewController()
+@interface ViewController() <UITextFieldDelegate>
 //MARK: UILabel
 @property(nonatomic, retain) UILabel *resultColorLabel;
 @property(nonatomic, retain) UILabel *redLabel;
@@ -15,6 +15,8 @@
 //MARK: UIView
 @property(nonatomic, retain) UIView *resultColorView;
 @property(nonatomic, retain) UIView *mainView;
+//MARK: -TextFieldArray
+@property(nonatomic, retain) NSArray<UITextField *> *textFields;
 @end
 
 @implementation ViewController
@@ -39,6 +41,13 @@
     self.resultColorView = [UIView new];
     self.mainView = [UIView new];
     
+    self.textFields = [NSArray new];
+    
+    //MARK: Delegate
+    self.textFieldRed.delegate = self;
+    self.textFieldGreen.delegate = self;
+    self.textFieldBlue.delegate = self;
+    
     //MARK: SetAccessibilityIdentifier
     self.mainView.accessibilityIdentifier = @"mainView";
     
@@ -52,20 +61,20 @@
     self.greenLabel.accessibilityIdentifier = @"labelGreen";
     self.blueLabel.accessibilityIdentifier = @"labelBlue";
     self.resultColorLabel.accessibilityIdentifier = @"labelResultColor";
-    
     self.resultColorView.accessibilityIdentifier = @"viewResultColor";
     
+    self.resultColorLabel.text = @"Color";
     //MARK: AssigningTextToElements
-    self.textFieldRed.placeholder = @" 0..255";
-    self.textFieldGreen.placeholder = @" 0..255";
-    self.textFieldBlue.placeholder = @" 0..255";
+    self.textFieldRed.placeholder = @"0..255";
+    self.textFieldGreen.placeholder = @"0..255";
+    self.textFieldBlue.placeholder = @"0..255";
     
     [self.processButton setTitle:@"Process" forState: UIControlStateNormal];
     
     self.redLabel.text = @"RED";
     self.greenLabel.text = @"GREEN";
     self.blueLabel.text = @"BLUE";
-    self.resultColorLabel.text = @"Color";
+    
     
     //MARK: AppearanceOfElement
     CGColorRef borderColor = [UIColor colorWithRed:0.2 green:0.2 blue:0.2  alpha:1].CGColor;
@@ -86,16 +95,25 @@
     
     [self.processButton setTitleColor:UIColor.systemBlueColor forState:UIControlStateNormal];
     
-    //MARK:
-    [self.processButton addTarget:self action:@selector(buttonAction:) forControlEvents:UIControlEventTouchUpInside];
+    self.mainView.translatesAutoresizingMaskIntoConstraints = NO;
+    NSArray<UIView*> *labelArray = @[self.resultColorLabel, self.redLabel, self.greenLabel, self.blueLabel];
+    self.textFields = @[
+        self.textFieldRed,
+        self.textFieldGreen,
+        self.textFieldBlue];
     
-    //MARK: AddConstraint
+    NSMutableArray *allViews = [NSMutableArray arrayWithArray:labelArray];
+    [allViews addObjectsFromArray:self.textFields];
+    [allViews addObjectsFromArray:@[self.resultColorView, self.processButton]];
+    for(UIView *view in allViews){
+        view.translatesAutoresizingMaskIntoConstraints = NO;
+    }
+    [self.view addSubview:self.mainView];
     
-    
-    
-    
-    
-    
+    [self.mainView.leadingAnchor constraintEqualToAnchor:self.view.leadingAnchor constant:0].active = YES;
+    [self.mainView.topAnchor constraintEqualToAnchor:self.view.topAnchor constant:0].active = YES;
+    [self.mainView.trailingAnchor constraintEqualToAnchor:self.view.trailingAnchor constant:0].active = YES;
+    [self.mainView.bottomAnchor constraintEqualToAnchor:self.view.bottomAnchor constant:0].active = YES;
     
     //MARK: AddSubview
     [self.mainView addSubview:self.textFieldRed];
@@ -103,36 +121,80 @@
     [self.mainView addSubview:self.textFieldBlue];
     [self.mainView addSubview:self.processButton];
     [self.mainView addSubview:self.redLabel];
-    [self.mainView addSubview:self.textFieldGreen];
-    [self.mainView addSubview:self.textFieldBlue];
+    [self.mainView addSubview:self.greenLabel];
+    [self.mainView addSubview:self.blueLabel];
     [self.mainView addSubview:self.resultColorLabel];
     [self.mainView addSubview:self.resultColorView];
+    
+    [self.processButton addTarget:self action:@selector(buttonAction:) forControlEvents:UIControlEventTouchUpInside];
+    
+    //MARK: AddConstraint
+    for(UIView *view in allViews){
+        [view.heightAnchor constraintEqualToConstant:30].active = YES;
+    }
+    
+    for(NSInteger i = 0; i < labelArray.count; i++){
+        if(!i){
+            [labelArray[i].leadingAnchor constraintEqualToAnchor:self.mainView.leadingAnchor constant:20].active = YES;
+            [labelArray[i].topAnchor constraintEqualToAnchor:self.mainView.topAnchor constant:60].active = YES;
+            [labelArray[i].widthAnchor constraintEqualToConstant:100].active = YES;
+            continue;
+        }
+        
+        [labelArray[i].widthAnchor constraintEqualToConstant:60].active = YES;
+        [labelArray[i].leadingAnchor constraintEqualToAnchor:labelArray[i-1].leadingAnchor constant:0].active = YES;
+        [labelArray[i].topAnchor constraintEqualToAnchor:labelArray[i-1].topAnchor constant:60].active = YES;
+    }
+    
+    //MARK: AddConstraint
+    [self.textFieldGreen.bottomAnchor constraintEqualToAnchor:self.greenLabel.bottomAnchor constant:0].active = YES;
+    [self.textFieldRed.bottomAnchor constraintEqualToAnchor:self.redLabel.bottomAnchor constant:0].active = YES;
+    [self.textFieldBlue.bottomAnchor constraintEqualToAnchor:self.blueLabel.bottomAnchor constant:0].active = YES;
+    
+    [self.textFieldGreen.leadingAnchor constraintEqualToAnchor:self.greenLabel.trailingAnchor constant:30].active = YES;
+    [self.textFieldRed.leadingAnchor constraintEqualToAnchor:self.textFieldGreen.leadingAnchor constant:0].active = YES;
+    [self.textFieldBlue.leadingAnchor constraintEqualToAnchor:self.textFieldGreen.leadingAnchor constant:0].active = YES;
+    
+    [self.textFieldGreen.trailingAnchor constraintEqualToAnchor:self.mainView.trailingAnchor constant:-30].active = YES;
+    [self.textFieldRed.trailingAnchor constraintEqualToAnchor:self.mainView.trailingAnchor constant:-30].active = YES;
+    [self.textFieldBlue.trailingAnchor constraintEqualToAnchor:self.mainView.trailingAnchor constant:-30].active = YES;
+    
+    [self.resultColorView.bottomAnchor constraintEqualToAnchor:self.resultColorLabel.bottomAnchor constant:0].active = YES;
+    [self.resultColorView.leadingAnchor constraintEqualToAnchor:self.resultColorLabel.trailingAnchor constant:30].active = YES;
+    [self.resultColorView.trailingAnchor constraintEqualToAnchor:self.mainView.trailingAnchor constant:-30].active = YES;
+    
+    [self.processButton.centerXAnchor constraintEqualToAnchor:self.mainView.centerXAnchor constant:0].active = YES;
+    [self.processButton.widthAnchor constraintEqualToConstant:100].active = YES;
+    [self.processButton.topAnchor constraintEqualToAnchor:self.blueLabel.bottomAnchor constant:30].active = YES;
+    
+    [self.processButton.centerXAnchor constraintEqualToAnchor:self.mainView.centerXAnchor constant:0].active = YES;
+    [self.processButton.widthAnchor constraintEqualToConstant:100].active = YES;
+    [self.processButton.topAnchor constraintEqualToAnchor:self.blueLabel.bottomAnchor constant:40].active = YES;
 }
 
-//MARK: -Action
+//MARK: TextFieldDidBeginEditing
+- (void)textFieldDidBeginEditing:(UITextField *)textField{
+    self.resultColorLabel.text = @"Color";
+}
+
+//MARK: - Action
 -(void) buttonAction:(UIButton *)sender{
-    NSString *redStr = self.textFieldRed.text;
-    NSString *greenStr = self.textFieldGreen.text;
-    NSString *blueStr = self.textFieldBlue.text;
-    NSMutableString *all = redStr.mutableCopy;
-    [all appendString:greenStr];
-    [all appendString:blueStr];
+    NSString *redValueString = self.textFieldRed.text;
+    NSString *greenValueString = self.textFieldGreen.text;
+    NSString *blueValueString = self.textFieldBlue.text;
+    NSMutableString *rgbColorString = [NSMutableString new];
+    [rgbColorString appendFormat:@"%@%@%@",redValueString,greenValueString,blueValueString];
     
-    [self.textFieldRed resignFirstResponder];
-    [self.textFieldGreen resignFirstResponder];
-    [self.textFieldBlue resignFirstResponder];
-    
-    self.textFieldRed.text = @"";
-    self.textFieldGreen.text = @"";
-    self.textFieldBlue.text = @"";
-    
-    if(redStr.length && greenStr.length && blueStr.length){
+    for (UITextField *textField in self.textFields) {
+          [textField resignFirstResponder];
+          textField.text = @"";
+      }
+    if(redValueString.length && greenValueString.length && blueValueString.length){
         NSCharacterSet* notDigits = [[NSCharacterSet decimalDigitCharacterSet] invertedSet];
-        if ([all rangeOfCharacterFromSet:notDigits].location == NSNotFound)
-        {
-            int red = redStr.intValue;
-            int green = greenStr.intValue;
-            int blue = blueStr.intValue;
+        if ([rgbColorString rangeOfCharacterFromSet:notDigits].location == NSNotFound) {
+            int red = redValueString.intValue;
+            int green = greenValueString.intValue;
+            int blue = blueValueString.intValue;
             if(red >=0 && red <= 255 && green >= 0 && green <= 255 && blue >= 0 && blue <= 255){
                 
                 self.resultColorLabel.text = [NSString stringWithFormat:@"0x%02X%02X%02X", red, green, blue];
@@ -143,7 +205,8 @@
         }
     }
     
-    //self.labelResultColor.text = @"Error";
-    //self.viewResultColor.backgroundColor = UIColor.clearColor;
+    self.resultColorLabel.text = @"Error";
+    self.resultColorView.backgroundColor = UIColor.clearColor;
 }
+
 @end
